552428e21be0184925b13e9725ea35bf40942af8
aflotten <anders3835@hotmail.com>
2024-02-28 22:40:55 -0700

    Completed styling, action text, alerts

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,7 @@
 class Meeting < ApplicationRecord
-  validates :body, presence: true
+  validates :title, presence: true
 
   after_create_commit { broadcast_prepend_to "meetings" }
   # after_update_commit { broadcast_replace_to "meetings" }
+
 end
